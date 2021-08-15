06d88157f3eb11c1f7f355397f20c7458110eddc
ronaldhexoid <ronald.hexooid@gmail.com>
2021-08-15 11:36:10 +0530

    first commit

---
+++ jirareport/polls/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
